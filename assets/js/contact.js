4d290a1436537d9f5ef58fd2680a25d3fa35af64
sandra smith <toosandra@gmail.com>
2024-03-12 17:32:55 -0400

    working thru contact

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,23 +1,16 @@
 let contactForm = document.getElementById('contact-form')
 
 
-(function () {
-    // https://dashboard.emailjs.com/admin/account
-    emailjs.init({
-        publicKey: "YOUR_PUBLIC_KEY",
-    });
-})();
-</script >
-    <script type="text/javascript">
-        window.onload = function() {
-            document.getElementById('contact-form').addEventListener('submit', function (event) {
-                event.preventDefault();
-                // these IDs from the previous steps
-                emailjs.sendForm('contact_service', 'contact_form', this)
-                    .then(() => {
-                        console.log('SUCCESS!');
-                    }, (error) => {
-                        console.log('FAILED...', error);
-                    });
+
+window.onload = function () {
+    contactForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+        // these IDs from the previous steps
+        emailjs.sendForm('contact_service', 'contact_form', '#contact-form')
+            .then(() => {
+                console.log('SUCCESS!', response.status, response.text);
+            }, (error) => {
+                console.log('FAILED...', error);
             });
+    });
 }
