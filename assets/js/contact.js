09e921041c4f29370f3f00a1550b5e40a91c8670
sandra smith <toosandra@gmail.com>
2024-04-01 00:02:02 -0400

    fixed email

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,14 +1,11 @@
-let contactForm = document.getElementById('contact-form')
 
-
-
-window.onload = function () {
-    contactForm.addEventListener('submit', function (event) {
+window.onload = function() {
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
         event.preventDefault();
         // these IDs from the previous steps
-        emailjs.sendForm('contact_service', 'contact_form', '#contact-form')
+        emailjs.sendForm('service_j2a5kdb', 'contact_form', this)
             .then(() => {
-                console.log('SUCCESS!', response.status, response.text);
+                console.log('SUCCESS!');
             }, (error) => {
                 console.log('FAILED...', error);
             });
